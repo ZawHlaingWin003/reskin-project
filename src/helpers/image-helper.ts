7278b0efb1a9da4f055b0f8e6e4e3239ecf4d299
ZawHlaingWin003 <zawhlaingwin003@gmail.com>
2024-11-24 17:14:38 +0700

    fix: Fixed dynamic url not showing bug

--- src/helpers/image-helper.ts
+++ src/helpers/image-helper.ts
@@ -1,12 +1,15 @@
-
 const getImageUrl = (image: string, { directory = '', fallback = '' } = {}) => {
     if (!image) {
         console.warn("Image path is undefined or empty. Using fallback image if provided.");
-        image = fallback;
+        return fallback ? new URL(`../assets/images/${fallback}`, import.meta.url).href : '';
     }
-    return new URL(`../assets/images/${directory ? `${directory}/` : ''}${image}`, import.meta.url).href;
-}
 
-export {
-    getImageUrl
-}
+    try {
+        return new URL(`../assets/images/${directory ? `${directory}/` : ''}${image}`, import.meta.url).href;
+    } catch (e) {
+        console.error(`Image not found: ${directory}/${image}. Using fallback if provided.`);
+        return fallback ? new URL(`../assets/images/${fallback}`, import.meta.url).href : '';
+    }
+};
+
+export { getImageUrl };
