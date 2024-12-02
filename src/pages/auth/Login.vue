08a1b50f41b0e11df5511e438d1dac16fdbe1f6a
ZawHlaingWin007 <zawhlaingwin.mti@gmail.com>
2024-12-02 20:08:31 +0700

    style: Animate login bg

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -3,22 +3,22 @@
 		<img
 			src="/assets/images/background/7.png"
 			alt="Bg"
-			class="absolute w-40 h-40 translate-x-1/2 top-full -left-1/2 -z-10 opacity-10"
+			class="absolute w-40 h-40 top-full -left-1/2 -z-10 opacity-10 animate-float delay-1"
 		>
 		<img
-			src="/assets/images/background/8.png"
+			src="/assets/images/background/5.png"
 			alt="Bg"
-			class="absolute left-0 w-40 h-40 -translate-x-1/2 -top-20 opacity-10"
+			class="absolute w-40 h-40 -left-1/2 -top-20 opacity-10 animate-float delay-2"
 		>
 		<img
 			src="/assets/images/background/12.png"
 			alt="Bg"
-			class="absolute right-0 w-40 h-40 translate-x-1/2 top-full -z-10 opacity-10"
+			class="absolute right-0 w-40 h-40 translate-x-1/2 top-full -z-10 opacity-10 animate-float delay-3"
 		>
 		<img
 			src="/assets/images/background/10.png"
 			alt="Bg"
-			class="absolute right-0 w-40 h-40 translate-x-1/2 -top-20 opacity-10"
+			class="absolute right-0 w-40 h-40 translate-x-1/2 -top-20 opacity-10 animate-float delay-4"
 		>
 		<div class="flex flex-col items-center justify-center mx-auto md:px-6">
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,29 @@ export default {
       fontFamily: {
         'onest': "Onest, sans-serif"
       },
+      animation: {
+        float: 'float 2s ease-in-out infinite', // Custom animation name with duration and infinite loop
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0) translateX(50%)' }, // Start and end positions
+          '50%': { transform: 'translateY(-10px) translateX(50%)' }, // Midway position
+        },
+      },
     },
   },
   plugins: [
-    primeui
+    primeui,
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.delay-0': { animationDelay: '0s' },
+        '.delay-1': { animationDelay: '1s' },
+        '.delay-2': { animationDelay: '2s' },
+        '.delay-3': { animationDelay: '3s' },
+        '.delay-4': { animationDelay: '4s' },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
   ]
 }
 
