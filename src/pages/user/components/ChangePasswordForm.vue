2970c4e64496c062c36755d64c883679c5a08e32
ZawHlaingWin007 <zawhlaingwin.mti@gmail.com>
2024-12-02 22:18:41 +0700

    style: Changed input style

--- src/pages/user/components/ChangePasswordForm.vue
+++ src/pages/user/components/ChangePasswordForm.vue
@@ -1,58 +1,58 @@
 <template>
-    <form
-        @submit.prevent="updatePasswordForm"
-        class="space-y-2 md:space-y-4"
-    >
-        <div>
-            <Password
-                v-model="form.oldPassword"
-                toggleMask
-                class="w-full"
-                :placeholder="t('profile.old_password')"
-                :feedback="false"
-                :invalid="!!errors.oldPassword"
-            />
-            <Message
-                size="small"
-                variant="simple"
-                severity="error"
-            ><span class="text-xs md:text-base">{{ errors.oldPassword }}</span></Message>
-        </div>
-        <div>
-            <Password
-                v-model="form.newPassowrd"
-                toggleMask
-                class="w-full"
-                :placeholder="t('profile.new_password')"
-                :feedback="false"
-                :invalid="!!errors.newPassowrd"
-            />
-            <Message
-                size="small"
-                variant="simple"
-                severity="error"
-            ><span class="text-xs md:text-base">{{ errors.newPassowrd }}</span></Message>
-        </div>
-        <div>
-            <Password
-                v-model="form.confirmPassword"
-                toggleMask
-                class="w-full"
-                :placeholder="t('profile.confirm_password')"
-                :feedback="false"
-                :invalid="!!errors.confirmPassword"
-            />
-            <Message
-                size="small"
-                variant="simple"
-                severity="error"
-            >{{ errors.confirmPassword }}</Message>
+    <form @submit.prevent="updatePasswordForm">
+        <div class="space-y-2 md:space-y-4">
+
+            <div>
+                <Password
+                    v-model="form.oldPassword"
+                    toggleMask
+                    class="w-full"
+                    :placeholder="t('profile.old_password')"
+                    :feedback="false"
+                    :invalid="!!errors.oldPassword"
+                />
+                <Message
+                    size="small"
+                    variant="simple"
+                    severity="error"
+                ><span class="text-xs md:text-base">{{ errors.oldPassword }}</span></Message>
+            </div>
+            <div>
+                <Password
+                    v-model="form.newPassowrd"
+                    toggleMask
+                    class="w-full"
+                    :placeholder="t('profile.new_password')"
+                    :feedback="false"
+                    :invalid="!!errors.newPassowrd"
+                />
+                <Message
+                    size="small"
+                    variant="simple"
+                    severity="error"
+                ><span class="text-xs md:text-base">{{ errors.newPassowrd }}</span></Message>
+            </div>
+            <div>
+                <Password
+                    v-model="form.confirmPassword"
+                    toggleMask
+                    class="w-full"
+                    :placeholder="t('profile.confirm_password')"
+                    :feedback="false"
+                    :invalid="!!errors.confirmPassword"
+                />
+                <Message
+                    size="small"
+                    variant="simple"
+                    severity="error"
+                >{{ errors.confirmPassword }}</Message>
+            </div>
         </div>
         <Button
             type="submit"
             :label="t('profile.update_password')"
             severity="danger"
-            class="w-full"
+            class="w-full mt-4"
         ></Button>
     </form>
 </template>
